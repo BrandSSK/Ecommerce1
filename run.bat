31c52f33e1fd5a4c097a592a90cb1daa5da954e0
BrandSSK <sudarshansk9@gmail.com>
2021-03-30 17:07:17 +0530

    Adding project

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Workspace\Ecommerce\Ecommerce
+mvn clean test
---
+++ src/test/java/com/Ecommerce/testCases/test.java
@@ -0,0 +1,11 @@
+package com.Ecommerce.testCases;
+
+import org.apache.poi.util.SystemOutLogger;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("tyest");
+	}
+
+}
